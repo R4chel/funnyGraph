402792e2b4ef4d90ade2af0f4bfc1b86dd2145d4
Rachel <rae84@cornell.edu>
2013-09-08 18:52:46 -0400

    working clean function and outline of process method

--- comedian_scraper.py
+++ comedian_scraper.py
@@ -1,11 +1,27 @@
 import scraperwiki
 import wikipedia_utils
+import re
 #wikipedia_utils = scraperwiki.utils.swimport("wikipedia_utils")
 
-title = "Aquamole Pot"
+def clean(s):
+    ''' remove references and comments '''
+    s = re.sub("<ref>([^<>])*</ref>","",s)
+    s = re.sub("<!--([^<>])*-->","",s)
+    return s
+
+title = "Cardis Cardell Willis"
 
 val = wikipedia_utils.GetWikipediaPage(title)
 res = wikipedia_utils.ParseTemplates(val["text"])
-print res               # prints everything we have found in the text
-infobox_ukcave = dict(res["templates"]).get("Infobox ukcave")
-print infobox_ukcave    # prints just the ukcave infobox
+infobox_comedian = dict(res["templates"]).get("Infobox comedian")
+influences = infobox_comedian.get("influences")
+influenced = infobox_comedian.get("influenced")
+print clean(influences)
+print clean(influenced)
+
+#print influences
+#print influenced
+#print infobox_comedian    # prints just the comedian infobox
+
+
+
